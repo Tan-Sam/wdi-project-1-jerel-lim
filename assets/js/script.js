f7bde749d1d920062a2a39e5be9c2a9f38f12bef
Jerel Lim <j.limster@gmail.com>
2017-03-30 12:59:34 +0800

    addressed comments, added more display, fixed win logic bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
   var buttonAdd = document.querySelector('.add')
   var buttonPass = document.querySelector('.pass')
   var buttonBet = document.querySelector('.bet')
-  var game = new Game()
+  var buttonPlay = document.querySelector('.play')
   var player1Cards = document.querySelectorAll('.player1 .cards')
   var computerCards = document.querySelectorAll('.computer .cards')
-  var creditsLeft = document.querySelector('h1')
+  var creditsLeft = document.querySelector('.container h1')
+  var game = new Game()
 
-  console.log(creditsLeft)
+// set up game constructor and prototypes
   function Game () {
     this.currentPlayer = 0
-    this.isGameOver = false
     this.player = []
     this.credits = 1000
-  }
-
-  function displayCards (player, num, div) {
-    buttonAdd.disabled = false
-    for (var j = 1; j < 53; j++) {
-      if (game.player[player].cards[num].card === j) {
-            // div[num].setAttribute('style', "background-color: white")
-              // div[num].style.backgroundColor = 'white';
-            div[num].setAttribute('style', "background-image: url('./assets/cards-img/" + j + '.png')
-
-      }
-    }
-  }
-
-  function removeDisplay (player, num, div) {
-    buttonAdd.disabled = true
-    player1Cards[0].removeAttribute('style')
-    player1Cards[1].removeAttribute('style')
-    computerCards[0].removeAttribute('style')
-    computerCards[1].removeAttribute('style')
   }
 
   Game.prototype.newGame = function () {
     buttonAdd.disabled = true
     buttonPass.disabled = true
+    buttonPlay.disabled = true
+    buttonBet.disabled = false
     this.player.push(new Player(), new Player())
-    // this.player[this.currentPlayer].bet()
     displayCards(0, 1, player1Cards)
     displayCards(0, 0, player1Cards)
     displayCards(1, 0, computerCards)
@@ -48,6 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   Game.prototype.switchPlayer = function () {
     buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
     if (this.currentPlayer === 0 && this.player[0].pass === 'false') {
       this.currentPlayer = 0
     } else {
@@ -59,11 +43,12 @@ document.addEventListener('DOMContentLoaded', function () {
   Game.prototype.restart = function () {
     buttonAdd.disabled = true
     buttonPass.disabled = true
+    buttonBet.disabled = false
+    buttonPlay.disabled = true
     setTimeout(function () {
       if (confirm('Play again?')) {
         removeDisplay()
         this.currentPlayer = 0
-        this.isGameOver = false
         this.player = []
         this.knownCards = []
         this.betAmount = 0
@@ -73,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
         for (var i = 0; i < newCards.length; i++) {
           newCards[i].parentNode.removeChild(newCards[i])
         }
-        alert('New game started, please enter your bet.')
+        alert('New round started, please enter your bet.')
       } else {
         alert('Have a good day!')
       }
@@ -81,40 +66,48 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   Game.prototype.whoWon = function () {
-    buttonAdd.disabled = false
+    buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
     if (this.player[1].cardscore > 21) {
       setTimeout(function () { alert('You Won!') }, 1300)
       game.credits = game.credits + game.betAmount
       updateCredits()
       game.restart()
-      console.log(game.credits, game.betAmount)
       return game.credits
-    } else if (this.player[1].cardscore > this.player[0].cardscore) {
+    } else if (this.player[0].cardscore > 21) {
       setTimeout(function () { alert('Computer Won!') }, 1300)
       game.credits = game.credits - game.betAmount
       updateCredits()
       game.restart()
-      console.log(game.credits, game.betAmount)
       return game.credits
-    } else if (this.player[0].cardscore > this.player[1].cardscore) {
+    } else if (this.player[1].cardscore < 22 && this.player[1].cardscore > this.player[0].cardscore) {
+      setTimeout(function () { alert('Computer Won!') }, 1300)
+      game.credits = game.credits - game.betAmount
+      updateCredits()
+      game.restart()
+      return game.credits
+    } else if (this.player[0].cardscore < 22 && this.player[0].cardscore > this.player[1].cardscore) {
       setTimeout(function () { alert('You Won!') }, 1300)
       game.credits = game.credits + game.betAmount
       updateCredits()
       game.restart()
-      console.log(game.credits, game.betAmount)
       return game.credits
     } else {
       setTimeout(function () { alert('It\'s a tie!') }, 1300)
       updateCredits()
       game.credits = game.credits
       game.restart()
-      console.log(game.credits)
       return game.credits
     }
   }
 
   Game.prototype.blackJack = function () {
-    buttonAdd.disabled = false
+    buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
     if (this.player[0].cards.length === 2 && this.player[0].cardscore === 21 && this.player[1].cards.length === 2 && this.player[1].cardscore === 21) {
       setTimeout(function () { alert('It\'s a tie!') }, 1300)
       displayCards(1, 1, computerCards)
@@ -122,7 +115,6 @@ document.addEventListener('DOMContentLoaded', function () {
       updateCredits()
       game.restart()
       game.credits = game.credits
-      console.log(game.credits)
       return game.credits
     } else if (this.player[0].cards.length === 2 && this.player[0].cardscore === 21) {
       setTimeout(function () { alert('You Won!') }, 1300)
@@ -131,7 +123,6 @@ document.addEventListener('DOMContentLoaded', function () {
       game.credits = game.credits + game.betAmount
       updateCredits()
       game.restart()
-      console.log(game.credits, game.betAmount)
       return game.credits
     } else if (this.player[1].cards.length === 2 && this.player[1].cardscore === 21) {
       setTimeout(function () { alert('Computer Won!') }, 1300)
@@ -140,74 +131,45 @@ document.addEventListener('DOMContentLoaded', function () {
       game.credits = game.credits - game.betAmount
       updateCredits()
       game.restart()
-      console.log(game.credits, game.betAmount)
       return game.credits
     }
   }
 
+// Card constructor
   function Card (card, suit, rank) {
     this.card = card // random number from 1-52
     this.suit = suit // 1-clubs, 2-diamonds,3-hearts,4-spades
     this.rank = rank  // 1-13, ace = 1, j = 11, q = 12, k = 13
   }
 
+// Player Constructor
   function Player (name) {
     this.name = name
     this.cards = [deal(), deal()]
     this.pass = false
     this.cardscore = this.score()
   }
-  var deal = function () {
-    buttonAdd.disabled = false // general action of dealing
-    var card = Math.floor(Math.random() * 52 + 1)
-    var rank = card % 13
-    var suit = card % 4
-    return new Card(card, suit, rank)
-  }
-
+// players passing
   Player.prototype.pass = function () {
     this.pass = true
     return this.pass
   }
+
+  // players getting additional cards
   Player.prototype.hit = function () {
     this.cards.push(deal())
     this.score()
     this.cards.forEach(function (card) {
-      console.log(this.cardscore)
       if (card.rank === 1 && this.cardscore > 21) {
-        console.log(card.rank)
-        console.log(this.cardscore)
         this.cardscore = this.cardscore - 10
-        console.log(this.cardscore)
       }
     }.bind(this))
     if (this.cardscore > 21) {
-      setTimeout(function () { alert('Computer Won!') }, 300)
-      game.credits = game.credits - game.betAmount
-      updateCredits()
-      game.restart()
-      console.log(game.credits)
-      return game.credits
+      return game.whoWon()
     }
   }
 
-  function bet () {
-    game.betAmount = parseInt(document.querySelector('input').value)
-    if (game.betAmount < game.credits && game.betAmount) {
-      console.log('game start')
-      game.newGame()
-      updateCredits()
-      console.log(game.betAmount)
-      buttonAdd.disabled = false
-      buttonPass.disabled = false
-      return game.betAmount
-    } else {
-      alert("Bet amount is invalid. What's your bet? You have " + game.credits + ' credits currently')
-    }
-
-    console.log('play game')
-  }
-
+  // updating player score
   Player.prototype.score = function () {
     var arrayValue = this.cards.map(function (card) {
       return value(card.rank)
@@ -219,6 +181,19 @@ document.addEventListener('DOMContentLoaded', function () {
     return this.cardscore
   }
 
+  // dealing the cards to players
+  var deal = function () {
+    buttonAdd.disabled = false
+    buttonPass.disabled = false
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
+    var card = Math.floor(Math.random() * 52 + 1)
+    var rank = card % 13
+    var suit = card % 4
+    return new Card(card, suit, rank)
+  }
+
+  // calculating score from cards dealt
   function value (rank) {
     if (rank === 11 || rank === 12 || rank === 0) {
       return 10
@@ -228,25 +203,83 @@ document.addEventListener('DOMContentLoaded', function () {
       return rank
     }
   }
+  // bet amount
+  function bet () {
+    game.betAmount = parseInt(document.querySelector('input').value)
+    if (game.betAmount <= game.credits && game.betAmount) {
+      game.newGame()
+      updateCredits()
+      displayChips()
+      buttonAdd.disabled = false
+      buttonPass.disabled = false
+      buttonBet.disabled = true
+      buttonPlay.disabled = true
+      return game.betAmount
+    } else {
+      buttonAdd.disabled = true
+      buttonPass.disabled = true
+      buttonBet.disabled = false
+      buttonPlay.disabled = true
 
+      alert("Bet amount is invalid. What's your bet? You have " + game.credits + ' credits currently')
+    }
+  }
+  // pass turn
+  function pass () {
+    buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
+    game.player[game.currentPlayer].pass = true
+    game.switchPlayer()
+  }
+// computer AI actions
   function computerAI () {
     buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
     displayCards(1, 1, computerCards)
     computerCards[1].removeAttribute('id')
     while (game.currentPlayer !== 0) {
       if (game.player[game.currentPlayer].cardscore < 16) {
         addCard()
+      } else if (game.player[game.currentPlayer].cardscore > 15 && game.player[game.currentPlayer].cardscore < 22) {
+        return game.whoWon()
       } else {
-        game.whoWon()
-        game.isGameOver = true
-        return game.isGameOver
+        return true
       }
     }
   }
 
+// displaying cards of players
+  function displayCards (player, num, div) {
+    buttonAdd.disabled = false
+    buttonPass.disabled = false
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
+    console.log('displaycard', 'bet', 'play')
+    div[num].setAttribute('style', "background-image: url('./assets/cards-img/" + game.player[player].cards[num].card + '.png')
+  }
+
+  // removing display of cards
+  function removeDisplay (player, num, div) {
+    buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonBet.disabled = false
+    buttonPlay.disabled = true
+    player1Cards[0].removeAttribute('style')
+    player1Cards[1].removeAttribute('style')
+    computerCards[0].removeAttribute('style')
+    computerCards[1].removeAttribute('style')
+  }
+
+  // adding card display and calling hit function upon clicking add button
   var addCard = function () {
     buttonAdd.disabled = false
     buttonPass.disabled = false
+    buttonBet.disabled = true
+    buttonPlay.disabled = true
     if (game.currentPlayer === 0) {
       var player1Div = document.querySelector('.player1')
       var newCard = document.createElement('div')
@@ -265,32 +298,44 @@ document.addEventListener('DOMContentLoaded', function () {
       displayCards(1, computerCards.length - 1, computerCards)
     }
   }
-
-  function pass () {
-    buttonAdd.disabled = true
-    game.player[game.currentPlayer].pass = true
-    game.switchPlayer()
-  }
-
+// updating display of credits left
   function updateCredits () {
     creditsLeft.innerHTML = 'Credits Left: ' + game.credits
   }
 
-  // game.newGame()
-  alert("What's your bet? You have " + game.credits + ' credits currently')
+// updating display of chips bet
+  function displayChips () {
+    var y = 38
+    var x = 44.5
+    var z = 0
+    for (i = 0; i < game.betAmount / 10; i++) {
+      var chiparea = document.querySelector('.chiparea')
+      var newChip = document.createElement('div')
+      newChip.classList.add('chips', 'newCards')
+      chiparea.appendChild(newChip)
+      y -= 4
+      z += 1
+      $('.chips:last-child').css({
+        'top': y + '%',
+        'left': x + '%',
+        'z-index': z
+      })
+    }
+  }
+
+// play game, instructions to game board function
+  function play () {
+    $('.instructions').hide()
+    $('.container').fadeIn()
+    setTimeout(function () { alert("What's your bet? You have " + game.credits + ' credits currently') }, 1000)
+    buttonAdd.disabled = true
+    buttonPass.disabled = true
+    buttonPlay.disabled = true
+  }
+
+  // buttons
   buttonAdd.addEventListener('click', addCard)
   buttonPass.addEventListener('click', pass)
   buttonBet.addEventListener('click', bet)
-
-  // console.log(game)
-  // console.log(game.player[0].cards)
-  // console.log(game.player[0].cardscore)
-  // console.log(game.player[1].cards)
-  // console.log(game.player[1].cardscore)
-
-  //
-  //
-
-  // }
-//
+  buttonPlay.addEventListener('click', play)
 })
